535fa04c575631da6097da2816661fe1e3404a79
sdgamer007 <sohaildua38@gmail.com>
2022-10-15 19:11:59 +0200

    some issues remain but tasks done

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -5,7 +5,7 @@ try:
     from airflow.operators.bash_operator import BashOperator
     from airflow.operators.python_operator import PythonOperator
     from airflow.operators.mysql_operator import MySqlOperator
-    
+    from airflow.operators.email_operator import EmailOperator
   
 
     print("All Dag modules are ok ......")
@@ -13,6 +13,8 @@ except Exception as e:
     print("Error  {} ".format(e))
 
 from datacleaner import data_cleaner
+
+yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 with DAG(
         dag_id="first_dag",
         schedule_interval="@daily",
@@ -31,5 +33,19 @@ with DAG(
     t4 = MySqlOperator(task_id='insert_into_table', mysql_conn_id="mysql_conn", sql="insert_into_table.sql")
      
     t5 = MySqlOperator(task_id='select_from_table', mysql_conn_id="mysql_conn", sql="select_from_table.sql")
-    t1 >> t2 >> t3 >> t4 >> t5
+    
+    t6 = BashOperator(task_id='move_file1', bash_command='cat ~/store_files_airflow/location_wise_profit.csv && mv ~/store_files_airflow/location_wise_profit.csv ~/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date)
+
+    t7 = BashOperator(task_id='move_file2', bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date)
+
+    t8 = EmailOperator(task_id='send_email',
+        to='example@gmail.com',
+        subject='Daily report generated',
+        html_content=""" <h1>Congratulations! Your store reports are ready.</h1> """,
+        files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date])
+
+    t9 = BashOperator(task_id='rename_raw', bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date)
+
+    t1 >> t2 >> t3 >> t4 >> t5 >> [t6,t7] >> t8 >> t9
+
     
